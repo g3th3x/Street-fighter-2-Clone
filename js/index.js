2fd164c378677c128940d3dc916cd2ee54086f60
g3th3x <g3th3x@gmail.com>
2022-09-24 14:48:36 +0300

    added images (ken, background)

--- js/index.js
+++ js/index.js
@@ -10,22 +10,38 @@ window.onload = function () {
   canvas.width = gameViewport.gvWidth;
   canvas.height = gameViewport.gvHeight;
 
-  const ken = document.querySelector("img");
+  const [ken, background] = document.querySelectorAll("img");
+
+  const position = {
+    x: gameViewport.gvWidth / 2 - ken.width / 2,
+    y: 110,
+  };
+
+  let velocity = 2;
 
   function frame() {
-    ctx.strokeStyle = "yellow";
-    ctx.moveTo(0, 0);
-    ctx.lineTo(gameViewport.gvWidth, gameViewport.gvHeight);
-    ctx.moveTo(gameViewport.gvWidth, 0);
-    ctx.lineTo(0, gameViewport.gvHeight);
-    ctx.stroke();
+    position.x += velocity;
 
-    ctx.drawImage(ken, 0, 0);
+    if (position.x < 0 || position.x > gameViewport.gvWidth - ken.width) {
+      velocity = -velocity;
+    }
+
+    // ctx.clearRect(0, 0, gameViewport.gvWidth, gameViewport.gvHeight);
+    ctx.drawImage(background, 0, 0);
+
+    // ctx.strokeStyle = "yellow";
+    // ctx.moveTo(0, 0);
+    // ctx.lineTo(gameViewport.gvWidth, gameViewport.gvHeight);
+    // ctx.moveTo(gameViewport.gvWidth, 0);
+    // ctx.lineTo(0, gameViewport.gvHeight);
+    // ctx.stroke();
+
+    ctx.drawImage(ken, position.x, position.y);
 
     window.requestAnimationFrame(frame);
   }
 
   window.requestAnimationFrame(frame);
 
-  console.log(ctx);
+  //   console.log(ctx);
 };
