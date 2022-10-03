4651cbb616e93721861c89ea116c3999a72e4de9
g3th3x <g3th3x@gmail.com>
2022-10-03 17:56:07 +0300

    extending the control scheme to accommodate gamepads

--- src/constants/control.js
+++ src/constants/control.js
@@ -1,3 +1,9 @@
+export const GamepadThumbstick = {
+  DEAD_ZONE: "deadZone",
+  HORIZONTAL_AXE_ID: "horizontalAxeId",
+  VERTICAL_AXE_ID: "verticalAxeId",
+};
+
 export const Control = {
   LEFT: "left",
   RIGHT: "right",
@@ -7,6 +13,16 @@ export const Control = {
 
 export const controls = [
   {
+    gamePad: {
+      [GamepadThumbstick.DEAD_ZONE]: 0.5,
+      [GamepadThumbstick.HORIZONTAL_AXE_ID]: 0,
+      [GamepadThumbstick.VERTICAL_AXE_ID]: 1,
+
+      [Control.LEFT]: "14",
+      [Control.RIGHT]: "15",
+      [Control.UP]: "12",
+      [Control.DOWN]: "13",
+    },
     keyboard: {
       [Control.LEFT]: "ArrowLeft",
       [Control.RIGHT]: "ArrowRight",
@@ -15,6 +31,16 @@ export const controls = [
     },
   },
   {
+    gamePad: {
+      [GamepadThumbstick.DEAD_ZONE]: 0.5,
+      [GamepadThumbstick.HORIZONTAL_AXE_ID]: 0,
+      [GamepadThumbstick.VERTICAL_AXE_ID]: 1,
+
+      [Control.LEFT]: "14",
+      [Control.RIGHT]: "15",
+      [Control.UP]: "12",
+      [Control.DOWN]: "13",
+    },
     keyboard: {
       [Control.LEFT]: "KeyA",
       [Control.RIGHT]: "KeyD",
