5dc0ab6bad816c787c775f116b999ae9c0b11771
g3th3x <g3th3x@gmail.com>
2022-10-07 11:52:00 +0300

    adding the countdown timer logic

--- src/entities/overlays/StatusBar.js
+++ src/entities/overlays/StatusBar.js
@@ -1,9 +1,18 @@
+import {
+  TIME_DELAY,
+  TIME_FRAME_KEYS,
+  TIME_FLASH_DELAY,
+} from "../../constants/battle.js";
+
 export class StatusBar {
   constructor(fighters) {
     this.image = document.querySelector('img[alt="misc"]');
 
     this.time = 99;
     this.timeTimer = 0;
+    this.timeFlashTimer = 0;
+    this.useFlashFrames = false;
+
     this.fighters = fighters;
 
     this.frames = new Map([
@@ -11,16 +20,27 @@ export class StatusBar {
 
       ["ko-white", [161, 16, 32, 14]],
 
-      ["time-0", [16, 32, 14, 16]],
-      ["time-1", [32, 32, 14, 16]],
-      ["time-2", [48, 32, 14, 16]],
-      ["time-3", [64, 32, 14, 16]],
-      ["time-4", [80, 32, 14, 16]],
-      ["time-5", [96, 32, 14, 16]],
-      ["time-6", [112, 32, 14, 16]],
-      ["time-7", [128, 32, 14, 16]],
-      ["time-8", [144, 32, 14, 16]],
-      ["time-9", [160, 32, 14, 16]],
+      [`${TIME_FRAME_KEYS[0]}-0`, [16, 32, 14, 16]],
+      [`${TIME_FRAME_KEYS[0]}-1`, [32, 32, 14, 16]],
+      [`${TIME_FRAME_KEYS[0]}-2`, [48, 32, 14, 16]],
+      [`${TIME_FRAME_KEYS[0]}-3`, [64, 32, 14, 16]],
+      [`${TIME_FRAME_KEYS[0]}-4`, [80, 32, 14, 16]],
+      [`${TIME_FRAME_KEYS[0]}-5`, [96, 32, 14, 16]],
+      [`${TIME_FRAME_KEYS[0]}-6`, [112, 32, 14, 16]],
+      [`${TIME_FRAME_KEYS[0]}-7`, [128, 32, 14, 16]],
+      [`${TIME_FRAME_KEYS[0]}-8`, [144, 32, 14, 16]],
+      [`${TIME_FRAME_KEYS[0]}-9`, [160, 32, 14, 16]],
+
+      [`${TIME_FRAME_KEYS[1]}-0`, [16, 192, 14, 16]],
+      [`${TIME_FRAME_KEYS[1]}-1`, [32, 192, 14, 16]],
+      [`${TIME_FRAME_KEYS[1]}-2`, [48, 192, 14, 16]],
+      [`${TIME_FRAME_KEYS[1]}-3`, [64, 192, 14, 16]],
+      [`${TIME_FRAME_KEYS[1]}-4`, [80, 192, 14, 16]],
+      [`${TIME_FRAME_KEYS[1]}-5`, [96, 192, 14, 16]],
+      [`${TIME_FRAME_KEYS[1]}-6`, [112, 192, 14, 16]],
+      [`${TIME_FRAME_KEYS[1]}-7`, [128, 192, 14, 16]],
+      [`${TIME_FRAME_KEYS[1]}-8`, [144, 192, 14, 16]],
+      [`${TIME_FRAME_KEYS[1]}-9`, [160, 192, 14, 16]],
 
       // Name tags
       ["tag-ken", [128, 56, 30, 9]],
@@ -47,7 +67,27 @@ export class StatusBar {
     ctx.setTransform(1, 0, 0, 1, 0, 0);
   }
 
-  update(time) {}
+  updateTime(time) {
+    if (time.previous > this.timeTimer + TIME_DELAY) {
+      //  if (this.time > 0) {
+      this.time -= 1;
+      //  }
+      this.timeTimer = time.previous;
+    }
+
+    if (
+      this.time < 15 &&
+      this.time > -1 &&
+      time.previous > this.timeFlashTimer + TIME_FLASH_DELAY
+    ) {
+      this.useFlashFrames = !this.useFlashFrames;
+      this.timeFlashTimer = time.previous;
+    }
+  }
+
+  update(time) {
+    this.updateTime(time);
+  }
 
   drawHealthBars(ctx) {
     this.drawFrame(ctx, "health-bar", 31, 20);
@@ -63,10 +103,11 @@ export class StatusBar {
   }
 
   drawTimer(ctx) {
-    const timeString = String(this.time).padStart(2, "00");
+    const timeString = String(Math.max(this.time, 0)).padStart(2, "00");
+    const flashFrame = TIME_FRAME_KEYS[Number(this.useFlashFrames)];
 
-    this.drawFrame(ctx, `time-${timeString.charAt(0)}`, 178, 33);
-    this.drawFrame(ctx, `time-${timeString.charAt(1)}`, 194, 33);
+    this.drawFrame(ctx, `${flashFrame}-${timeString.charAt(0)}`, 178, 33);
+    this.drawFrame(ctx, `${flashFrame}-${timeString.charAt(1)}`, 194, 33);
   }
 
   draw(ctx) {
