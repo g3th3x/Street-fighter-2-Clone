2c7ff87ced2714ff1b75a34c63cafa40aeb0c90d
g3th3x <g3th3x@gmail.com>
2022-11-04 02:03:35 +0300

    adding the boat bobbing animation

--- src/entities/Stage.js
+++ src/entities/Stage.js
@@ -1,3 +1,4 @@
+import { FRAME_TIME } from "../constants/game.js";
 import { drawFrame } from "../utils/context.js";
 
 export class Stage {
@@ -9,12 +10,53 @@ export class Stage {
       ["stage-boat", [8, 16, 521, 180]],
       ["stage-floor", [8, 392, 896, 72]],
     ]);
+
+    this.boat = {
+      position: { x: 0, y: 0 },
+      animationFrame: 0,
+      animationTimer: 0,
+      animationDelay: 22,
+      animation: [0, -1, -2, -3, -4, -3, -2, -1],
+    };
   }
 
-  update() {}
+  updateBoat(time) {
+    if (
+      time.previous >
+      this.boat.animationTimer + this.boat.animationDelay * FRAME_TIME
+    ) {
+      this.boat.animationTimer = time.previous;
+      this.boat.animationFrame += 1;
+      this.boat.animationDelay = 22 + (Math.random() * 16 - 8);
+    }
+
+    if (this.boat.animationFrame >= this.boat.animation.length) {
+      this.boat.animationFrame = 0;
+    }
+  }
+
+  update(time) {
+    this.updateBoat(time);
+  }
 
   drawFrame(ctx, frameKey, x, y) {
     drawFrame(ctx, this.image, this.frames.get(frameKey), x, y);
+  }
+
+  drawBoat(ctx, camera) {
+    this.boat.position = {
+      x: Math.floor(150 - camera.position.x / 1.613445),
+      y: Math.floor(
+        -camera.position.y + this.boat.animation[this.boat.animationFrame]
+      ),
+    };
+
+    this.drawFrame(
+      ctx,
+      "stage-boat",
+      this.boat.position.x,
+      this.boat.position.y
+    );
   }
 
   draw(ctx, camera) {
@@ -24,12 +66,7 @@ export class Stage {
       Math.floor(16 - camera.position.x / 2.157303),
       -camera.position.y
     );
-    this.drawFrame(
-      ctx,
-      "stage-boat",
-      Math.floor(150 - camera.position.x / 1.613445),
-      -1 - camera.position.y
-    );
+    this.drawBoat(ctx, camera);
     this.drawFrame(
       ctx,
       "stage-floor",
